8b58088d70922c4fc91ee305362e0d6b8a4e0075
Thy Nguyen <minhthy0120032003@gmail.com>
2020-10-30 10:58:18 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "styles/main/theme.sass";
+import "styles/main/App.sass";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <p className="test">
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="app-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
